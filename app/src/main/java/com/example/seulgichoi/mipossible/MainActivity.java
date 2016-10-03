553e1f11b61168277ed35174746b56a2b536cab4
SeulgiEricChoi <choislgi1@gmail.com>
2016-10-03 16:39:52 +0900

    Add WebView event

--- app/src/main/java/com/example/seulgichoi/mipossible/MainActivity.java
+++ app/src/main/java/com/example/seulgichoi/mipossible/MainActivity.java
@@ -5,32 +5,49 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        web = (WebView) this.findViewById(R.id.web1);
+        web.setWebViewClient(new WebViewClient());
+
+        web.loadUrl("http://m.daum.net");
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn3 = (Button) findViewById(R.id.button3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DayActivity.class);
-                startActivity(intent);
+                web.goBack();
             }
         });
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MonthActivity.class);
-                startActivity(intent);
+                web.reload();
             }
         });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.goForward();
+            }
+        });
+
     }
 }
